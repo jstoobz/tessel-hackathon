db44fadfbbc2d0446e2bd568b9aa1213320e4561
jstoobz <stephensja7@gmail.com>
2018-10-02 16:46:12 -0400

    brrp

--- camera/camera.js
+++ camera/camera.js
@@ -1,13 +1,22 @@
-var av = require('tessel-av');
-var os = require('os');
-var http = require('http');
-var port = 8000;
-var camera = new av.Camera();
+'use strict';
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const os = require('os');
+const path = require('path');
+const port = 8888;
 
-http
-  .createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'image/jpg' });
+const av = require('tessel-av');
+const camera = new av.Camera();
 
-    camera.capture().pipe(response);
-  })
-  .listen(port, () => console.log(`http://${os.hostname()}.local:${port}`));
+server.listen(port, function() {
+  console.log(`http://${os.hostname()}.local:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.get('/stream', (request, response) => {
+  console.log(camera.url);
+  response.redirect(camera.url);
+});
+
+module.exports = camera;
